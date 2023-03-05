fb9bffad409c283ca72cecd51635dfd2d69a4b60
Christopher Wong <cj1997wong@gmail.com>
2023-03-05 15:30:01 -0800

    Taking input images, added stb_image library.
    Added three image types: jpg, jfif and the png converted jfif. Looked around and saw a few libraries but noticed two videos on stb_image being good for image processing so I'm running with them. Not too sure if I need to focus too much on CMake at the moment but may review later for testing/running. Have to use a wierd mix of MSBuild with cmake to have it run correctly. Tutorial said install is the way so I can run it as a normal function like ls but can't seem to get it to work just yet. Still setting things up.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,39 @@
 
 // Christopher Wong
 // Just trying to get it to take in a picture.
-int main(){
+
+
+#include <iostream>
+
+// Trying out the stb_image library. From: https://github.com/nothings/stb
+
+/* Learning it from these 
+        https://www.youtube.com/watch?v=1OyQoPCp46o
+        https://www.youtube.com/watch?v=028GNYC32Rg&list=PLG5M8QIx5lkzdGkdYQeeCK__As6sI2tOY&index=1
+*/
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        std::cout << "ERROR: The image argument wasn't found." << std::endl;
+        exit(1);
+    }
+    if (argc > 2){
+        std::cout << "ERROR: Too many arguments (Max: 2) for image." << std::endl;
+        exit(1);
+    }  
+    int width, height, channels;
+    unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
+    if(!img){
+        std::cout << "ERROR: The image did not load in." << std::endl;
+        exit(2);
+    }
+    std::cout << "Image loaded." << std::endl;
+    std::cout << "The parameters of the loaded image are:" << std::endl;
+    std::cout << "\tHeight: " << height << "\n\tWidth: " << width << "\n\tChannels: " << channels << std::endl; 
+    stbi_image_free(img);
     return 0;
 }
