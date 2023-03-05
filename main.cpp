d181ede7771e11f9e9fa66aecc03da9923135da4
Christopher Wong <cj1997wong@gmail.com>
2023-03-05 15:55:55 -0800

    Added makefile, moved files into folders
    Definitely going to have to look into it. But for now I'm just going to have to do the runs in the makefile until I understand cmake better. Added the makefile and put the images and the stb headers into their own folders.

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@
         https://www.youtube.com/watch?v=028GNYC32Rg&list=PLG5M8QIx5lkzdGkdYQeeCK__As6sI2tOY&index=1
 */
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "./stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "./stb_image/stb_image_write.h"
 
 int main(int argc, char* argv[]){
     if(argc < 2){
---
+++ makefile
@@ -0,0 +1,5 @@
+default:
+	MSBuild.exe .\build\mazeAlgorithmStudy.sln
+	.\build\Debug\mazeAlgorithmStudy.exe images/bluish_colors-wren_@_sleeprealms.jfif
+	.\build\Debug\mazeAlgorithmStudy.exe images/bluish_colors-wren_@_sleeprealms.jpg
+	.\build\Debug\mazeAlgorithmStudy.exe images/bluish_colors-wren_@_sleeprealms.png
