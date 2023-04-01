48052643c6fd80e501d9986a55be88a019322ba5
Christopher Wong <cj1997wong@gmail.com>
2023-04-01 14:18:23 -0700

    Finished write/output.
    Can output files now. Had to readjust how I was taking in an argument to break it into pieces to put bakc together for the output file. Now gotta knuckle through trying to access individual pixel data.

--- main.cpp
+++ main.cpp
@@ -18,24 +18,78 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "./stb_image/stb_image_write.h"
 
-int main(int argc, char* argv[]){
-    if(argc < 2){
+int main(int argc, char* argv[]){    
+    if(argc < 2) {
         std::cout << "ERROR: The image argument wasn't found." << std::endl;
         exit(1);
     }
-    if (argc > 2){
+    if (argc > 2) {
         std::cout << "ERROR: Too many arguments (Max: 2) for image." << std::endl;
         exit(1);
     }  
+
+    std::string argument = argv[1], fPath = "", fName = "", fExt = "";
+    int linkAddress = 0;
+
+    for(int i = argument.length() - 1; i > -1; i--) {
+        if(!linkAddress) {
+            if(argument[i] == '.') {
+                linkAddress++;
+                continue;
+            }
+            else if(argument[i] == '\\' || argument[i] == '/') {
+                std::cout << "ERROR: Unrecognized Filetype" << std::endl;
+                exit(1);
+            }
+            fExt = argument[i] + fExt;
+        }
+        else if(linkAddress == 1) {
+            if(argument[i] == '\\' || argument[i] == '/'){
+                linkAddress++;
+                fPath += argument[i];
+                continue;
+            }
+            fName = argument[i] + fName;
+        }
+        else
+            fPath = argument[i] + fPath;
+    }
+    fExt = '.' + fExt;
+    
+    if(fExt != ".jfif" && fExt != ".png" && fExt != ".jpg") {
+        std::cout << "ERROR: Improper filetype: " << fExt << ". Only verified .jfif .png .jpg" << std::endl;
+        exit(1);
+    }
+
+
+    
     int width, height, channels;
-    unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
+    unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 3);
+
     if(!img){
         std::cout << "ERROR: The image did not load in." << std::endl;
         exit(2);
     }
-    std::cout << "Image loaded." << std::endl;
-    std::cout << "The parameters of the loaded image are:" << std::endl;
+
+    std::cout << "Image loaded.\nThe parameters of the loaded image are:" << std::endl;
     std::cout << "\tHeight: " << height << "\n\tWidth: " << width << "\n\tChannels: " << channels << std::endl; 
+    std::cout << "File Location Details: \n\tFile Path: " << fPath << "\n\tFile Name: " << fName << "\n\tFile Extension: " << fExt << std::endl; 
+
+    size_t img_size = width * height * channels;
+    fName = fPath + "changed_" + fName + fExt;
+    const char* outFile = fName.c_str();
+
+    if(fExt == ".jfif"){
+    }
+    else if(fExt == ".png"){
+
+    }
+    else if(fExt == ".jpg"){
+        std::cout << "File Detected: .jpg | Output file named: " << fName << std::endl;
+        stbi_write_jpg(outFile, width, height, channels, img, 100);
+    }
+
+    // delete(outFile);
     stbi_image_free(img);
     return 0;
 }
